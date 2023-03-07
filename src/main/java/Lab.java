d54d8b78c7f1af84f1db0fb55f38f4a17bcfc709
schorij23 <j_schori@aol.com>
2023-03-07 21:17:29 +0000

    Commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -30,7 +30,7 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        String str = "";
+        String str = new String();
         for(int i = 0; i < nums.length; i++){
             str += nums[i] + " ";
         }
